15ee8ab8216c32959871b8d3cf1067f691f83189
Herman <haavind30@gmail.com>
2024-09-09 22:52:37 +0200

    almost working

--- testSpektralMetode/test.jl
+++ testSpektralMetode/test.jl
@@ -1,23 +1,35 @@
+using Plots, LinearAlgebra
 
 
-function get_matrix(α, β, r)
+function get_matrix(N, α, β, r)
 
-    off_diag = -1*ones(N-1)*(α+β)
-    diag = ones(N)*(1 + α)
+    off_diag = -1*ones(N-2)*(α+β)
+    diag = ones(N-1)*(1 + α)
 
-    return Tridiagonal(off_diag.*r[1:N-1], diag, off_diag.*r[2:N])
+    off_diaglo = off_diag
+    off_diaglo[1:end] = off_diaglo[1:end]./r[2:N-1]
+
+
+
+
+    return Tridiagonal(off_diaglo, diag, off_diag./r[3:N])
 
 
 end
 
 function initialcondition(r)
 
-    return r.^2 .+5 
+    return r.^2 .+3
 
 end
 
+origoInterpolasjon(u) = (1/3)*(4*u[2] + u[3]) #må tilpasses for funksjoner som ikke er konstante for alle theta
 
-function initialilize(N, J, T, d_t, D)
+#u_originLaplace(u, d_r) = u[2] - ((d_r^2)/4)*u[1] 
+
+
+
+function initialilize(N, J, d_t, D)
 
     r = range(0, 1, length=N)
 
@@ -25,19 +37,17 @@ function initialilize(N, J, T, d_t, D)
 
     d_r = r[2] - r[1]
 
-    t = StepRange(0, d_t, T)
-
     α = (D*d_t)/(d_r^2)
 
     β = (D*d_t)/(2*d_r)
 
-    γ = 4*(d_t^2)/(d_r^2*D^2)
+    #γ = 4*(d_t^2)/(d_r^2*D^2)
 
-    A = get_matrix(α, β, r)
+    A = get_matrix(N, α, β, r)
 
-    B = get_matrix(-α, -β, r)
+    B = get_matrix(N, -α, -β, r)
 
-    return r, Θ, t, A, B, d_t, d_r, α, β, γ
+    return r, Θ, A, B, d_r
 
 
 end
@@ -45,21 +55,37 @@ end
 
 function main(N, J, T, d_t, D)
 
-    r, Θ, t, A, B, d_t, d_r, α, β, γ = initialilize(N, J, T, d_t, D)
+    r, Θ, A, B , d_r = initialilize(N, J, d_t, D)
 
-    u0 = initialcondition(r)
+    u = zeros(N, T)
 
-    A[1, 1] = 1
-    A[1, 2] = 0
-    A[end, end] = 5
+    u[:, 1] = initialcondition(r)
 
-    B[1, 1] = 2 + 4*γ(u0[2]- 1)
-    B[1, 2] = 0
 
-    
+    A[end, end] *= 5
+
+    B[end, end] *= 5
+
+
+    for i in 1:T-1
+        u[2:end, i+1] = (A\B)*u[2:end, i]
+
+        u[1, i+1] = origoInterpolasjon(u[:, i+1])
+    end
+
+    return u, r, Θ
  
 
 end
+
+
+
+u , r, Θ = main(10, 10, 10, 1E-7, 1)
+
+print(u)
+
+plot(u[end, :])
+
 
 
 
