a1e254bb5749a047b3e5ec80cc35762be4f62323
Herman <haavind30@gmail.com>
2024-10-01 16:46:13 +0200

    further development on 2d

--- testSpektralMetode/eulerMultidim.jl
+++ testSpektralMetode/eulerMultidim.jl
@@ -5,19 +5,24 @@ function multidimmatrix(N::Int, M::Int, α::Float64, β::Float64, γ::Float64, r
 
     S = N*M
 
-    diag = ones(S)*(1 -2* α)
 
-    off_diagup = ones(S-1)*(γ)
-    off_diaglo = ones(S-1)*(γ)
+    beta_array = β*ones(S)./r
+
+    gamma_array = γ*ones(S)./(r)^2
+
+    diag = ones(S)*(1 -2* α) .+ 2 .*gamma_array
+
+    off_diagup = ones(S-1).*gamma_array[1:end-1]
+    off_diaglo = ones(S-1).*gamma_array[2:end]
 
     m_diaglo = ones(S-M)*(α)
 
     m_diagup = ones(S-M)*(α)
 
-    periodic_intup = zeros(S-(M-1)) # migth not work for all
+    periodic_intup = zeros(S-(M-1))
 
-    periodic_intlo = zeros(S-(M-1)) # migth not work for all
-    diag[(N*(M-1)):end] .= 1
+    periodic_intlo = zeros(S-(M-1)) 
+
 
     for i in M:M:S
 
@@ -36,9 +41,31 @@ function multidimmatrix(N::Int, M::Int, α::Float64, β::Float64, γ::Float64, r
     
     end
 
-    diag[(N*(M-1)):end] .= 1
+    diag[S-M+1:end] .= 1
 
-    return A
+    off_diagup[S-(M-1):end] .= 0
+
+    off_diaglo[S-(M-1):end] .= 0
+
+    m_diagup[M+1:end] .= 0
+
+    m_diaglo[M+1:end] .= 0
+
+    periodic_intlo[end] = 0
+
+    periodic_intup[end] = 0
+
+    print(m_diaglo)
+
+    
+    #print lenth of all arrays
+
+    print(length(diag), length(off_diagup), length(off_diaglo), length(m_diagup), length(m_diaglo), length(periodic_intup), length(periodic_intlo))
+
+    
+
+
+    return spdiagm(0 => diag, 1 => off_diagup, -1 => off_diaglo, M => m_diagup, -(M) => m_diaglo, -(M-1) => periodic_intup, M-1 => periodic_intlo)
 
 
 end
@@ -46,6 +73,7 @@ end
 
 
 A = multidimmatrix(3, 4, 1., 1., 1., 0:10:1)
+
 
 
 heatmap(A)
