b8872f1f4b7ebf4cf91a9b90363f8468599603c0
Herman <haavind30@gmail.com>
2024-10-04 16:13:09 +0200

    dj

--- testSpektralMetode/test.jl
+++ testSpektralMetode/test.jl
@@ -1,4 +1,4 @@
-using Plots, LinearAlgebra
+using Plots, LinearAlgebra, Statistics
 
 
 function get_matrix(N, α, β, r)
@@ -36,11 +36,19 @@ function initialcondition(N)
 end
 
 
-function initialcondition2(r)
-    return r.^2 .+ 4
+function initialcondition2(r, θ)
+
+    u1 = zeros(length(r), length(θ))
+
+    for j in eachindex(θ)
+        u1[:, j] = (r.^2).*cos(θ[j]).+ 4
+    end
+
+    return  u1
+
 end
 
-origoInterpolasjon(u) = (1/3)*(4*u[2] - u[3])
+origoInterpolasjon(u) = (1/3)*(4*mean(u[2, :]) - mean(u[3, :]))
 
 #u_originLaplace(u, d_r) = u[2] - ((d_r^2)/4)*u[1] 
 
@@ -87,15 +95,18 @@ function main(N, J, T, d_t, D)
 
     r, Θ, A, B , d_r = initialilize(N, J, d_t, D)
 
-    u = zeros(N, T)
+    u = zeros(N, J, T)
 
-    u[:, 1] = initialcondition2(r)
+    u[:, :, 1] = initialcondition2(r, Θ)
 
 
     for i in 1:T-1
-        u[2:end, i+1] = (A\B)*u[2:end, i]
+        for j in 1:J
+            u[2:end, j, i+1] = (A\B)*u[2:end, j ,i]
 
-        u[1, i+1] = origoInterpolasjon(u[:, i+1])
+            u[1, :,  i+1] .= origoInterpolasjon(u[:, :, i+1])
+        end
+
     end
 
     return u, r, Θ
@@ -103,14 +114,28 @@ function main(N, J, T, d_t, D)
 
 end
 
+function gridtransform(r, θ, N, M)
+
+    x = zeros(N*M)
+    y = zeros(N*M)
+
+    for i in 1:N
+        for j in 1:M
+            x[j+(i-1)*M] = r[i]*cos(θ[j])
+            y[j+(i-1)*M] = r[i]*sin(θ[j])
+        end
+    end
+
+    return x, y
+
+end
 
 
-u , r, Θ = main(100, 10, 10000, 1E-3, 1)
+
+@time u , r, Θ = main(100, 100, 10000, 1E-4, 1)
+
+X, Y = gridtransform(r, Θ, 100, 10)
 
 
-plot(u[:, 1])
-plot!(u[:, 100])
-
-
-
+heatmap(u[:, :, 1], proj =:polar)
 
