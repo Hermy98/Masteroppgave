a7013d2a6a9e39ef0bdea13640fc350b5adc9a3d
Herman <haavind30@gmail.com>
2025-02-16 14:42:13 +0100

    omstrukturering

---
+++ spektralmetode/plotting_utility.jl
@@ -0,0 +1,141 @@
+
+prefactor(L::Int, n::Int) = (2*π * n)/L
+
+
+function prefactors(Lx::Int,N::Int)
+    
+    return [prefactor(Lx, n) for n in 0:N+1 ]
+
+end
+
+
+function divergence(ux, uy, x, y, T, K , μ,  energy)
+
+    divergence_t = deepcopy(ux)
+
+    if energy == true
+
+        defenergy_t = deepcopy(ux)
+
+    end
+
+    for i in 1:T
+
+
+        ux_spline = Spline2D(x, y, ux[:, :, i])
+
+        uy_spline = Spline2D(x, y, uy[:, :, i])
+
+        ux_deriv = derivative(ux_spline, x, y, nux = 1, nuy = 0)
+
+        uy_deriv = derivative(uy_spline, x, y, nux = 0, nuy = 1)
+
+        divergence_t[:, :, i] = ux_deriv + uy_deriv
+
+
+        if energy == true
+
+            ux_yderiv = derivative(ux_spline, x, y, nux = 0, nuy = 1)
+
+            uy_xderiv = derivative(uy_spline, x, y, nux = 1, nuy = 0)
+
+            defenergy_t[:, :, i] = (1/2)* K * (ux_deriv + uy_deriv).^2 + μ *((1/2)*(ux_yderiv+uy_xderiv) - (ux_deriv + uy_deriv)).^2
+
+        end
+
+
+    end
+
+
+    if energy == true
+
+        return divergence_t, defenergy_t
+
+    else
+
+        return divergence_t
+
+    end
+
+end
+
+
+
+
+
+
+function animation_2d(u_x, u_y, x, y, numiter, framerate)
+
+    fig = Figure(size = (800, 800))
+
+    ax1 = Axis(fig[1, 1])
+
+    iplot = Observable(1)
+
+    ux = @lift(u_x[:, :, $iplot])
+
+    uy = @lift(u_y[:, :, $iplot])
+
+    GLMakie.arrows!(x, y, ux, uy, arrowsize = 10, lengthscale = 0.1)
+
+    display(fig)
+
+
+
+    # for i in 1:numiter
+
+    #     iplot[] = i
+
+    #     sleep(0.05)
+
+    # end 
+
+
+
+    record(fig, "animation.mp4", 1:10:numiter; framerate = framerate) do i
+      iplot[] = i
+
+      sleep(0.05)
+
+    end
+
+
+end
+
+
+function animation_1d(u, x, y, numiter, framerate)
+
+    fig2 = Figure(size = (800, 800))
+
+    ax2 = Axis(fig2[1, 1], aspect=  1)
+
+    iplot1d = Observable(1)
+
+    min = minimum(u)
+
+    max = maximum(u)
+
+    uplot = @lift(u[:, :, $iplot1d])
+
+    GLMakie.heatmap!(x, y, uplot, colorrange = (min, max), colormap = :jet1)
+
+    GLMakie.Colorbar(fig2[1, 2], limits = (min, max), colormap = :jet1)
+
+    display(fig2)
+
+    # for i in 1:numiter
+
+    #     iplot1d[] = i
+
+    #     sleep(0.05)
+    
+    # end
+
+    record(fig2, "animation3.mp4", 1:10:numiter; framerate = framerate) do i
+     iplot1d[] = i
+
+     sleep(0.05)
+
+    end
+
+end
---
+++ spektralmetode/polarization.jl
@@ -0,0 +1,192 @@
+include("spektralmetode.jl")
+
+
+function polarisationtimederivs(polarisatincoefficients, Dmatrix, factor, λ, y, N)
+
+    for n in 1:N+1
+
+        fn_doublederiv, gn_doublederiv = interpolatderivpolar(polarisatincoefficients, y, n)
+
+        Dmatrix[:, 1, n] = λ*( -(factor[n]^2) * polarisatincoefficients[:, 1, n] + fn_doublederiv)
+
+        Dmatrix[:, 2, n] = λ*( -(factor[n]^2) * polarisatincoefficients[:, 2, n] + gn_doublederiv)
+
+
+    return Dmatrix
+
+    end
+
+end
+
+# function elastictimederivs(elcoefficientmatrix, Kmatrix, factor, K, μ, y, n)
+
+#     an_deriv, bn_deriv, cn_deriv, dn_deriv, an_doublederiv, bn_doublederiv, cn_doublederiv, dn_doublederiv = initerpolatderivate(elcoefficientmatrix, y, n)
+
+#     Kmatrix[2:end-1, 1,  n] =  μ * an_doublederiv[2:end-1] - (K + μ) * (factor[n])^2 * elcoefficientmatrix[2:end-1, 1, n] + K  * factor[n] * dn_deriv[2:end-1]
+
+#     Kmatrix[2:end-1, 2, n] = μ * bn_doublederiv[2:end-1] - (K + μ) * (factor[n])^2 * elcoefficientmatrix[2:end-1, 2, n]  - K  * factor[n] * cn_deriv[2:end-1]
+
+#     Kmatrix[2:end-1, 3, n] = (K +  μ) * cn_doublederiv[2:end-1] - μ * (factor[n])^2 * elcoefficientmatrix[2:end-1, 3, n] + K * factor[n] * bn_deriv[2:end-1]
+
+#     Kmatrix[2:end-1, 4, n] = (K + μ)* dn_doublederiv[2:end-1] - μ * (factor[n])^2 * elcoefficientmatrix[2:end-1, 4, n] - K * factor[n] * an_deriv[2:end-1] 
+
+#     return Kmatrix
+
+
+# end
+
+# function runge_kuttastep(elcoefficientmatrix, polarisationtioncoefficients, Dmatrix, Kmatrix, N, factor, K, μ, λ,  y)
+
+#     for i in 1:N+1
+
+#         Dmatrix[:, :, i] = polarisationtimederivs(polarisationtioncoefficients, Dmatrix, factor, λ, y, i)
+
+#         #Kmatrix[:, :, i] = elastictimederivs(elcoefficientmatrix, Kmatrix, factor, K, μ, y, i)
+
+
+#     end
+    
+#     return Dmatrix#, Kmatrix
+
+# end
+
+function initploarcoefficients(N, m, ϕ)
+
+    polarisationtioncoefficients = zeros(m, 2, N+1)
+
+    for i in 1:m
+
+        ft_ϕ = fft(ϕ[:, i, 1])
+
+        polarisationtioncoefficients[i, 1, :] = real.(ft_ϕ)[1:N+1]
+
+        polarisationtioncoefficients[i, 2, :] = imag.(ft_ϕ)[1:N+1]
+
+
+    end
+
+    return polarisationtioncoefficients
+
+end
+
+
+function interpolatderivpolar(polarisatincoefficients, y, n)
+
+    #println(n)
+
+    spilnes = [Spline1D(y, polarisatincoefficients[:, j, n]) for j in 1:2]
+
+
+    derivativ = [derivative(spilne, y, nu =2) for spilne in spilnes]
+
+    return derivativ[1], derivativ[2]
+
+end
+
+
+function initialcondition(N, F, m, y, Ly, T)
+
+    ϕ = zeros(2*N+1, m, T)
+    
+
+    for i in 1:2*N+1
+
+        ϕ[i, :, 1] =  F * sin.(((2 * π )/Ly)*y)
+
+        # ϕ[10, 10, 1] = 1000        
+
+    end
+
+    return ϕ
+
+end
+
+function calculate_ϕ(polarisationtioncoefficients, N, Lx, x, m)
+
+    ϕ_step = zeros(2*N+1, m)
+
+    for j in 1:m
+        
+        for i in 0:N
+            ϕ_step[:, j] += polarisationtioncoefficients[j, 1, i+1] * cos.(((2 * π * i)/Lx)*x ) + polarisationtioncoefficients[j, 2, i+1] * sin.(((2 * π * i)/Lx)*x )
+        end
+    end
+
+    return (1/(2*N+1))*ϕ_step
+end
+
+
+function runge_kutta_polarization(polarisatincoefficients, Dmatrix, N, factor, λ, y, dt)
+
+
+
+    Dmatrix[1] = polarisationtimederivs(polarisatincoefficients, Dmatrix[1], factor, λ, y, N)
+
+    Dmatrix[2] = polarisationtimederivs(polarisatincoefficients + dt/2 * Dmatrix[1], Dmatrix[2], factor, λ, y, N)
+
+    Dmatrix[3] = polarisationtimederivs(polarisatincoefficients + dt/2 * Dmatrix[2], Dmatrix[3], factor, λ, y, N)
+
+    Dmatrix[4] = polarisationtimederivs(polarisatincoefficients + dt * Dmatrix[3], Dmatrix[4], factor, λ, y, N)
+
+    polarisatincoefficients += dt/6 * (Dmatrix[1] + 2 * Dmatrix[2] + 2 * Dmatrix[3] + Dmatrix[4])
+
+
+    return polarisatincoefficients
+
+end
+
+
+
+function initalize(dy::Float64, N::Int, Lx::Int, Ly::Int, F::Float64, T::Int)
+
+    x = LinRange(-Lx/2, Lx/2, 2*N+1)
+
+    y = -Ly/2:dy:Ly/2
+
+    m = length(y)
+
+    ϕ = initialcondition(N, F, m, y, Ly, T)
+
+    factors = prefactors(Lx, N+1)
+
+    polarisationtioncoefficients = initploarcoefficients(N, m, ϕ)
+
+    D1matrix = zeros(m, 2, N+1)
+
+    D2matrix = deepcopy(D1matrix)
+
+    D3matrix = deepcopy(D1matrix)
+
+    D4matrix = deepcopy(D1matrix)
+
+    Dmatrix = [D1matrix, D2matrix, D3matrix, D4matrix]
+
+    return x, y, m, ϕ, factors, polarisationtioncoefficients, Dmatrix
+
+end
+
+function runsystem(N::Int, dy::Float64, Lx::Int, Ly::Int, F::Float64, T::Int, dt::Float64, λ::Float64)
+
+    x, y, m, ϕ, factors, polarisationtioncoefficients, Dmatrix = initalize(dy, N, Lx, Ly, F, T)
+
+
+    for i in 2:T
+
+        polarisationtioncoefficients = runge_kutta_polarization(polarisationtioncoefficients, Dmatrix, N, factors, λ, y, dt)
+
+        ϕ[:, :, i] = calculate_ϕ(polarisationtioncoefficients, N, Lx, x, m)
+
+    end
+
+    return x, y, ϕ
+
+end
+
+
+@time x, y, ϕ = runsystem(10, 1., 40, 20, 10. ,10000, 0.001, 0.1)
+
+
+#heatmap(x, y, ϕ[:, :, 1])
+
+animation_2d(cos.(ϕ), sin.(ϕ), x, y, 10000, 30)
+
